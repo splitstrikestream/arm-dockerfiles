0826c4604231147adfc9b9d96b7ccfe9de9a6d63
Split Strike Stream <split.strike.stream@protonmail.com>
2020-06-04 19:52:03 -0400

    Scripts now can be run from any dirs

--- arm64v8-alpine-tor-irssi/build.sh
+++ arm64v8-alpine-tor-irssi/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+_REL_DIRECTORY="`dirname \"$0\"`"
+_ABS_DIRECTORY="`( cd \"$_REL_DIRECTORY\" && pwd )`"
+_CURRDIR=$(pwd)
+
+cd ${_ABS_DIRECTORY}
 docker build -t splitstrikestream/arm64v8-alpine-tor-irssi .
+cd ${_CURRDIR}
--- arm64v8-alpine-tor-irssi/update_irssi_version.sh
+++ arm64v8-alpine-tor-irssi/update_irssi_version.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -eo pipefail
 
+_REL_DIRECTORY="`dirname \"$0\"`"
+_ABS_DIRECTORY="`( cd \"$_REL_DIRECTORY\" && pwd )`"
+_CURRDIR=$(pwd)
+
+cd ${_ABS_DIRECTORY}
 current="$(git ls-remote --tags https://github.com/irssi/irssi.git | cut -d/ -f3 | cut -d^ -f1 | sort -uV | grep -vE -- '-rc|-git$|-dev$' | tail -1)"
-
 set -x
-
 sed -ri 's/^(ENV IRSSI_VERSION) .*/\1 '"$current"'/' Dockerfile
+cd ${_CURRDIR}
